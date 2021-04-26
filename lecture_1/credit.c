6ad89ecd48f6f9cc55bc2ebb1caf7f7869eca3bf
Temu4 <artem.vorobjov@gmail.com>
2021-04-26 13:29:48 +0300

    fix print_card_logo

--- lecture_1/credit.c
+++ lecture_1/credit.c
@@ -36,13 +36,13 @@ bool check_sum(long number) {
 }
 
 void print_card_logo(long number){
-    if  ((number >= 34e13 && number < 35e13) || (number >= 37e13 && number < 38e13)){
+    if ((number >= 34e13 && number < 35e13) || (number >= 37e13 && number < 38e13)) {
          printf("AMEX\n");
-    }
-    if  (number >= 51e14 && number < 56e14){
+    } else if  (number >= 51e14 && number < 56e14) {
          printf("MASTERCARD\n");
-    }
-        if  ((number >= 4e12 && number < 5e12) || (number >= 4e15 && number < 5e15)){
+    } else if  ((number >= 4e12 && number < 5e12) || (number >= 4e15 && number < 5e15)) {
          printf("VISA\n");
+    } else {
+        printf("INVALID\n");
     }
 }
